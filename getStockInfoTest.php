9af7df7a2105d76158880d85be33c5faf9fadf49
roger.k.trussell@gmail.com <roger.k.trussell@gmail.com>
2018-12-31 09:49:31 -0500

    Update getStockInfoTest.php
    Delete reference to an unused variable and delete reference to the wrong HTTP content type, namely, "html/text"

--- getStockInfoTest.php
+++ getStockInfoTest.php
@@ -2,9 +2,7 @@
 	$currentPrice = (rand(1,100) + (rand(1,99) / 100));
 	$output = array("latestPrice" => $currentPrice);
 	header("Access-Control-Allow-Origin: http://rktlebnhwebworks.net:3000");
-	header("Last-Modified: " . $format_string); 
 	header("cache-control: no-cache, must-revalidate, max-age=0");
-	header("Content-Type: text/html; charset=utf-8");
 	header("Content-Type: application/json");
 	echo json_encode($output, JSON_FORCE_OBJECT);
 ?>
